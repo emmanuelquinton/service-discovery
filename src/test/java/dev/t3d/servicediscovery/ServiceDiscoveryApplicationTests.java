7540bcc3cc2bfc6800092998610ab1ccffca03ad
Emmanuel <equinton.pro@gmail.com>
2024-01-01 19:49:00 +0100

    create discovery service with eureka

---
+++ src/test/java/dev/t3d/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.t3d.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
